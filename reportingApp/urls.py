39e07126554190b1e2fd12d8c1a97772176a66dc
C0deGeek007 <sk773870@gmail.com>
2023-06-04 20:18:25 +0530

    loop/enhancement/class based views

--- reportingApp/urls.py
+++ reportingApp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("csv/upload/stores", views.uploadStoresFromCsv, name="uploadStoresCsv"),
-    path("csv/upload/businesshours", views.uploadBusinessFoursFromCsv, name="uploadBusinessFoursFromCsv"),
-    path("csv/upload/status", views.uploadStatusfromcsv, name="uploadStatusfromcsv"),
-    path("", views.uploadStatusfromcsv, name="uploadStatusfromcsv"),
+    path("csv/upload/stores", views.uploadStoresFromCsv.as_view(), name="uploadStoresCsv"),
+    path("csv/upload/businesshours", views.uploadBusinessFoursFromCsv.as_view(), name="uploadBusinessFoursFromCsv"),
+    path("csv/upload/status", views.uploadStatusfromcsv.as_view(), name="uploadStatusfromcsv"),
+    path("", views.uploadStatusfromcsv.as_view(), name="uploadStatusfromcsv"),
 ]
--- reportingApp/views.py
+++ reportingApp/views.py
@@ -2,61 +2,57 @@ import csv
 from itertools import islice
 
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
 from .models import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-@api_view(['POST'])
-def uploadStoresFromCsv(request):
-    file = request.FILES['file']
-
-    # let's check if it is a csv file
-    if not file.name.endswith('.csv'):
-        return HttpResponse(request, 'THIS IS NOT A CSV FILE')
-
-    decoded_file = file.read().decode('utf-8').splitlines()
-    reader = csv.DictReader(decoded_file)
-
-    objs = []
-    batch_size = 1000
-    for row in reader:
-        print(type(row['store_id']))
-        print(type(row['timezone_str']))
-        objs.append(Store(store_id= int(row['store_id']), timezone_str=row['timezone_str']))
-
-    while True:
-        batch = list(islice(objs, batch_size))
-        if not batch:
-            break
-        Store.objects.bulk_create(batch, batch_size)
-
-    return HttpResponse("Successful.")
-
-
-@api_view(['POST'])
-def uploadBusinessFoursFromCsv(request):
-    try:
+class uploadStoresFromCsv(APIView):
+    
+    def post(self, request):
         file = request.FILES['file']
         # let's check if it is a csv file
         if not file.name.endswith('.csv'):
             return HttpResponse(request, 'THIS IS NOT A CSV FILE')
+
         decoded_file = file.read().decode('utf-8').splitlines()
         reader = csv.DictReader(decoded_file)
 
-    except Exception as e:
-        print("Exception: ", e)
-        return HttpResponse("Some exception occoured.")
+        objs = []
+        for row in reader:
+            objs.append(Store(store_id= int(row['store_id']), timezone_str=row['timezone_str']))
+
+        Store.objects.bulk_create(objs)
+
+        return Response("Successful.")
+
+class uploadBusinessFoursFromCsv(APIView):
+
+    def post(self, request):
+        try:
+            file = request.FILES['file']
+            # let's check if it is a csv file
+            if not file.name.endswith('.csv'):
+                return HttpResponse(request, 'THIS IS NOT A CSV FILE')
+            decoded_file = file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(decoded_file)
+
+        except Exception as e:
+            print("Exception: ", e)
+            return HttpResponse("Some exception occoured.")
+    
 
 
-@api_view(['POST'])
-def uploadStatusfromcsv(request):
-    try:
-        file = request.FILES['file']
-        # let's check if it is a csv file
-        if not file.name.endswith('.csv'):
-            return HttpResponse(request, 'THIS IS NOT A CSV FILE')
-        decoded_file = file.read().decode('utf-8').splitlines()
-        reader = csv.DictReader(decoded_file)
 
-    except Exception as e:
-        print("Exception: ", e)
-        return HttpResponse("Some exception occoured.")
+class uploadStatusfromcsv(APIView):
+    def post(self, request):
+        try:
+            file = request.FILES['file']
+            # let's check if it is a csv file
+            if not file.name.endswith('.csv'):
+                return HttpResponse(request, 'THIS IS NOT A CSV FILE')
+            decoded_file = file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(decoded_file)
+
+        except Exception as e:
+            print("Exception: ", e)
+            return HttpResponse("Some exception occoured.")
